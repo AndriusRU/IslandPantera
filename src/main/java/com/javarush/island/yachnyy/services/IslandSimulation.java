819503ba4e09c78e9991d15ab84c533c2f000430
Ячный Андрей Павлович <andrey.yachniy@ifellow.ru>
2026-05-13 10:57:25 +0700

    Carcas for map, animals finished

--- src/main/java/com/javarush/island/yachnyy/services/IslandSimulation.java
+++ src/main/java/com/javarush/island/yachnyy/services/IslandSimulation.java
@@ -3,10 +3,9 @@ package com.javarush.island.yachnyy.services;
 import com.javarush.island.yachnyy.entity.map.GameMap;
 
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class IslandSimulation {
 
-    private final GameMap map;
+//    private final GameMap map;
     private final AtomicInteger tact = new AtomicInteger(0);
 }
